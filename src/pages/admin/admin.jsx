d9489b11acbfed20ec2a054b4617d8685bb379de
Zeng-Dong-Song <3112460595@qq.com>
2020-08-06 13:15:45 +0800

    Saving before ejecting

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,11 @@
+/*
+用户登陆的路由组件
+*/
+import React, {Component} from 'react'
+export default class Login extends Component {
+    render () {
+        return (
+            <div>login</div>
+        )
+    }
+}
